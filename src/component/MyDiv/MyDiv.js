d8e1c9d5c35dfb2536b479afcd8690cb1bd01a8e
Aref Mokhtari <36030333+arefmokhtari@users.noreply.github.com>
2022-10-16 15:44:13 +0330

    add part_5 files !

---
+++ src/component/MyDiv/MyDiv.js
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+import MyP from './MyP';
+import './MyDiv.css';
+
+
+const MyDiv = props => <div className='my-div'>
+        <h4>name: <code>{props.name}</code></h4>
+        <h4>lastname: <code>{props.lastname}</code></h4>
+        <MyP style={{color: 'gray'}}>{props.children || 'test'}</MyP>
+    </div>;
+
+
+export default MyDiv;
