db2c00966dd0dab94a0e42de5ffa5f1a603689ae
arefmokhtari <aref100am@gmail.com>
2022-11-08 17:46:34 +0330

    add yup lib

--- src/components/ProductControl/ProductBuilder/ProductBuilder.js
+++ src/components/ProductControl/ProductBuilder/ProductBuilder.js
@@ -2,11 +2,10 @@
 import React from 'react';
 import './ProductBuilder.css';
 import RenderCM from '../../../hoc/RenderCM';
-const ProductBuilder = ({title, content}) => {
-    return <>
-        <p>title : {title}</p>
-        <p>content : {content}</p>
-    </>;
-}
+const ProductBuilder = ({title, content}) => <>
+    <p>title : {title}</p>
+    <p>content : {content}</p>
+</>;
+
 
 export default RenderCM(ProductBuilder, {className: 'product-builder'});
