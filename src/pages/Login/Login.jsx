24da6b983d31c18dd0d2ab4006ad20c5de48313e
Aref Mokhtari <36030333+arefmokhtari@users.noreply.github.com>
2022-12-25 20:19:18 +0330

    4 test, add span absolute

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,22 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 import { Button } from '@mui/material';
+import styled from 'styled-components'
 import { useFormik } from 'formik';
 import InputFromLogin from '../../components/InputFromLogin/InputFromLogin';
 import Temp4SignLogin from '../../components/Temp4SignLogin/Temp4SignLogin';
 import EmailIcon from '../../components/UI/ICONS/EmailIcon/EmailIcon';
 import PasswdIcon from '../../components/UI/ICONS/PasswdIcon/PasswdIcon';
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+const SpanSignUp = styled.span({
+    position: 'absolute',
+    bottom: '10px',
+    textAlign: 'center',
+    right: 0,
+    fontSize: '14px',
+    left: 0,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+});
 
 const Login = () => {
     // - - - - - - - - - - - - - - //
@@ -30,12 +41,11 @@ const Login = () => {
                 <InputFromLogin type='password' {... formik.getFieldProps('password')} label='رمز عبور' Icon={<PasswdIcon />} />
                 <Button type='submit' className='styleButtonHand' sx={{display: 'block !important', margin: 'auto'}} variant='contained'>ورود</Button>
             </form>
+            <SpanSignUp>آیا ثبت نام کرده اید؟ ثبت نام</SpanSignUp>
         </Temp4SignLogin>
     );
     // - - - - - - - - - - - - - - //
 }
-
-
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 export default Login;
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
